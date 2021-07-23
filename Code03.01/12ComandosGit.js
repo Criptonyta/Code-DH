35300f800fb5c3df8d82128c8adca298ca9ae74c
Criptonyta <daninavazagmail.com>
2021-07-23 20:21:37 -0300

    Adicionados comandos de Git

---
+++ Code03.01/12ComandosGit.js
@@ -0,0 +1,16 @@
+
+// PARA CREAR UN REPOSITORIO LOCAL
+// git config user.name 'nombreUsuario' // agrega nuestra identidad
+// git config user.email 'emailUsuario' // agrega nuestro e-mail
+// git init // crea el repositorio
+// git remote add origin http:... // apunta al repositorio remoto
+
+// PARA SUBIR CAMBIOS
+// git add . // agrega todos los archivos
+// git commit -m 'mensaje' // commitea los cambios hechos
+// git push origin master // envia los cambios al repositorio remoto
+// git status // realiza un seguimiento de los estados de los archivos
+
+// PARA DESCARGAR LOS CAMBIOS
+// git pull // descarga los cambios que existen en el repositorio remoto
+// git clone linkDelRepo // clonamos el repositorio en nuestra maquina
