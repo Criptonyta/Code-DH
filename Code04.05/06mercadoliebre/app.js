15ba300cc60babfd9e0a8e82f909702369ad5cce
Criptonyta <daninavaza@gmail.com>
2021-08-10 18:22:15 -0300

    Add: Ejercicio Mercadoliebre Flexbox

---
+++ Code04.05/06mercadoliebre/app.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const path = require('path');
+
+const app = express();
+
+app.use(express.static(path.resolve(__dirname,'./public')));
+
+app.listen(3000, () => console.log('Servidor corriendo en puerto 3000'));
+
+app.get('/', (req,res) => {
+    res.sendFile(path.resolve(__dirname,'./views/home.html'));
+})
