6c7c0b447b56741933c9f8d7f8dba8806ae3a01a
Criptonyta <daninavaza@gmail.com>
2021-08-19 14:22:58 -0300

    Mod: Finalizando ejercicio 04.05.07mercadolievre

--- Code04.05/07mercadoliebre/app.js
+++ Code04.05/07mercadoliebre/app.js
@@ -1,11 +1,19 @@
 const express = require("express");
 const app = express();
 const path = require("path");
-app.use(express.static(path.join(__dirname,"./public")));
+app.use(express.static(path.join(__dirname, "./public")));
+
+
+
 app.get("/", (req, res) => {
-  res.sendFile(path.join(__dirname,"./view/index.html"));
+  res.sendFile(path.join(__dirname, "./view/index.html"));
 });
 
-app.listen(3000, () => {
-  console.log("Server is running on PORT : 3000");
-});
+
+
+const port = process.env.PORT || '5000';
+
+app.listen(port,()=>{
+
+console.log(`Server is runnig in the Port : ${port}`);
+})
