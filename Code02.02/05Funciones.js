86eb733f49d054cb0729d5a2231ce23ff71d8d11
Criptonyta <daninavaza@gmail.com>
2021-09-02 17:57:16 -0300

    Mod: Adicionando definicion de funcion declarativa vs expresada y su diferencia en 02.02.05

--- Code02.02/05Funciones.js
+++ Code02.02/05Funciones.js
@@ -21,3 +21,31 @@ function saludar(){
 };
 console.log(mensaje);
 console.log(saludar());
+
+
+
+// . Diferencias entre una funcion declarada y expresada?
+// . Cual es mas eficiente y porque?
+
+// Funciones Declarativas:
+// En las funciones declarativas, utilizamos la palabra reservada function al inicio para poder declarar la función:
+
+function saludar(nombre) {
+	console.log(`Hola ${nombre}`);
+}
+saludar('Diego');
+
+// Función Expresada:
+// En la expresión de función, la declaración se inicia con la palabra reservada var, donde se generará una variable que guardará una función anónima.
+
+var nombre = function(nombre){
+    console.log(`Hola ${nombre}`)
+}
+nombre('Diego');
+
+// En la funcion expresada, la función podría o no llevar nombre, aunque es más común que se hagan anónimas.
+
+// Diferencias:
+// A las funciones declarativas se les aplica hoisting, y a la expresión de función, no. Ya que el hoisting solo se aplica en las palabras reservadas var y function.
+
+// Lo que quiere decir que con las funciones declarativas, podemos mandar llamar la función antes de que ésta sea declarada, y con la expresión de función, no, tendríamos que declararla primero, y después mandarla llamar.
