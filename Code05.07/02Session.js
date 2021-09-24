bbdfd01e66e68a0e3b65941a19d67dfb228830a4
Criptonyta <daninavaza@gmail.com>
2021-09-24 01:56:05 -0300

    terminando 05.07.02

--- Code05.07/02Session.js
+++ Code05.07/02Session.js
@@ -48,10 +48,111 @@ let userController = {
             <input type="email" name="email"><br>
             <label for="">Password</label>
             <input type="password" name="password"><br>
-
+            <input type="submit" value="Login">
         </form>
     </body>
 </html>
+*/
+
+// d) como el pedido tiene que viajar por post, voy a routes>users.js y:
+
+router.post('/login', [ //creo la ruta y valido la informacion con express validator
+    check('email').isEmail().withMessage("Email invalido"),
+    check('password').isLength({
+        min: 8
+    }).withMessage('La contraseña debe tener un minimo de 8 caracteres')
+], usersController.processLogin)
+
+// e) de vuelta en usersControllers para crear la logica del process login
+
+let userController = {
+    register: function (req, res) {
+        return res.render('register')
+    },
+
+    login: function (req, res) {
+        return res.render('login')
+    },
+
+    processLogin: function (req, res) {
+        let errors = validationResults(req); //primero le pregunto si hay errores
+
+        if (errors.isEmpty()) { //ahora pregunto si el usuaro existe y la pass es correcta
+            let userJSON = fs.readFyleSync('users.json', { // este pedazo de codigo trae a los usuarios 
+                en // (ojo que hay codigo perdido entre las llaves)
+            })
+            let users;
+            if (usersJSON == "") {
+                users = [];
+            } else {
+                users = JSON.parse(usersJSON);
+            }
+
+            for (let i = 0; i < users.length; i++) { // voy recorriendo uno a uno si el usuario existe
+                if (users[i].email == req.body.email) { //pregunto si el mail es el del user que se acaba de logar
+                    if (bcrypt.compareSync(req.body.password, users[i].password)) //la contrasena es valida?
+                        let usuarioALoguearse = users[i]; // despues del chequeo, veo si es el usuario que encontre
+                    break;
+                }
+            }
+            if (usuarioALoguearse == undefined) {
+                return res.render('login', {
+                    errors: [{
+                        msg: 'Credenciales invalidas'
+                    }]
+                })
+
+                req.session.usuarioLogueado = usuarioALoguearse //guardo el usuario para que lo autorice a entrar
+                res.render('success'); //le envio al usuario el mensaje de exito
+            } else {
+                return res.render('login', { // si hay un error, devuelve al login compartiendo los errores
+                    errors: errors.errors
+                })
+            }
+        }
+    }
+}
+
+// f) como guarde en session los datos del usuario que se acaba de logar, voy nuevamente a routes>users.js para crear una ruta y que lo puedan ver tambien en otros lugares, creando una ruta para verificar si estas logueado
+
+router.get('/check', function (req, res) {
+    if (req.session.usuarioLogueado == undefined) {
+        res.send('No estas logueado');
+    } else {
+        res.send("el usuario logueado es: " + req.session.usuarioLogueado.email);
+    }
+})
+
+// g) una vez que estas logueado, no se va a volver a loguear y le voy a dar acceso a otras paginas de la app. Creo un middleware para eso. Creo dos archivos a) authMiddleware (se encargara de chequear si el usuario esta logueado) y b) guestmiddleware ( se encargara de cuando no esta logueado)
+
+// a) authMiddleware
+
+function authMiddleware(req, res, next) {
+    if (req.session.usuarioLogueado != undefined) {
+        next();
+    } else {
+        res.send('Esta pagina es solo para usuarios')
+    }
+}
+
+module.exports = authMiddleware
 
 
-completar desde el minuto 11 del video */
+// b) guestMiddleware
+
+function guestMiddleware(req, res, next) {
+    if (req.session.usuarioLogueado == undefined) {
+        next();
+    } else {
+        res.send('Esta pagina es solo para invitados')
+    }
+}
+
+module.exports = guestMiddleware
+
+
+// h) vuelvo a routes> users para importar los modulos que cree y para decirce que la ruta de registracion solo estara disponible si sos invitado
+
+let guestMiddleware = require('../middlewares/guestMiddleware');
+
+router.get('/register', guestMiddleware, usersController.register)
