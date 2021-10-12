7de06aa821942b1126bc7bfc4d84ab1cc2772b13
Criptonyta <daninavaza@gmail.com>
2021-10-11 22:16:23 -0300

    Mod: Pequenos ajustes en el entregable individual

--- Code06.03/09entregableIndividual/script.sql
+++ Code06.03/09entregableIndividual/script.sql
@@ -61,16 +61,16 @@ SELECT * FROM usuarios;
 -- Creando los datos en notas
 
 INSERT INTO notas (note_id, id_user, note_title, note_content, blocked_note, creation_date, mod_date, del_date)
-VALUES (DEFAULT, 1, 'Lista de compras', '1) Comprar verdura 2) Queso 3) Leche 4) Yogurt', 0, '2020-01-01 10:10:10', '2020-01-01 10:10:10', '0'),
-(DEFAULT, 1, 'Cosas para hacer', '1) Lavar el auto 2) Contabilidad del mes 3) Pasear al perro 4) Hacer compras', 1, '2020-01-01 10:10:10', '2020-01-01 10:10:10', 0),
-(DEFAULT, 2, 'Lista de estudio', '1) Matematica 2) Fisica 3) Programacion 4) Ingles', 1, '2020-01-01 10:10:10', '2020-01-01 10:10:10', 0),
-(DEFAULT, 3, 'Materias 2* semestre', '1) Biologia 2) Sociologia 3) Arte', 0, '2020-01-01 10:10:10', '2020-01-01 10:10:10', 0),
-(DEFAULT, 5, 'Pendientes del trabajo', '1) Cerrar cuentas banco 2) Reunion con el equipo 3) Coordinar con los gerentes el plan de vacunacion 4) Programa de yoga en el trabajo', 1, '2020-01-01 10:10:10', '2020-01-01 10:10:10', 0),
-(DEFAULT, 6, 'Peliculas favoritas', '1) Mi malvado preferido 2) Buenos muchachos 3) El Padrino', 1, '2020-01-01 10:10:10', '2020-01-01 10:10:10', 0),
-(DEFAULT, 7, 'Libros para comprar', '1) Heroes y tumbas 2) El hombre y la maquina 3) Armaggeddon', 0, '2020-01-01 10:10:10', '2020-01-01 10:10:10', 0),
-(DEFAULT, 8, 'Musicos favoritos', '1) Talking Heads 2) AC/DC 3) Divididos 4) Seru Giran', 0, '2020-01-01 10:10:10', '2020-01-01 10:10:10', 0),
-(DEFAULT, 9, 'Lista de compras', '1) Pan 2) Manteca 3) Polenta 4) Yogurt', 0, '2020-01-01 10:10:10', '2020-01-01 10:10:10', 0),
-(DEFAULT, 10, 'Cumpleanos', '1) 26/06 = Juanito 2) 11/05 = Menganito 3) 01/04 = Felipe', 0, '2020-01-01 10:10:10', '2020-01-01 10:10:10', 0);
+VALUES (DEFAULT, '1', 'Lista de compras', '1) Comprar verdura 2) Queso 3) Leche 4) Yogurt', '0', '2020-01-01 10:10:10', '2020-01-01 10:10:10', '0'),
+(DEFAULT, '1', 'Cosas para hacer', '1) Lavar el auto 2) Contabilidad del mes 3) Pasear al perro 4) Hacer compras', 1, '2020-01-01 10:10:10', '2020-01-01 10:10:10', '0'),
+(DEFAULT, '2', 'Lista de estudio', '1) Matematica 2) Fisica 3) Programacion 4) Ingles', '1', '2020-01-01 10:10:10', '2020-01-01 10:10:10', '0'),
+(DEFAULT, '3', 'Materias 2* semestre', '1) Biologia 2) Sociologia 3) Arte', '0', '2020-01-01 10:10:10', '2020-01-01 10:10:10', '0'),
+(DEFAULT, '5', 'Pendientes del trabajo', '1) Cerrar cuentas banco 2) Reunion con el equipo 3) Coordinar con los gerentes el plan de vacunacion 4) Programa de yoga en el trabajo', '1', '2020-01-01 10:10:10', '2020-01-01 10:10:10', '0'),
+(DEFAULT, '6', 'Peliculas favoritas', '1) Mi malvado preferido 2) Buenos muchachos 3) El Padrino', 1, '2020-01-01 10:10:10', '2020-01-01 10:10:10', '0'),
+(DEFAULT, '7', 'Libros para comprar', '1) Heroes y tumbas 2) El hombre y la maquina 3) Armaggeddon', 0, '2020-01-01 10:10:10', '2020-01-01 10:10:10', '0'),
+(DEFAULT, '8', 'Musicos favoritos', '1) Talking Heads 2) AC/DC 3) Divididos 4) Seru Giran', '0', '2020-01-01 10:10:10', '2020-01-01 10:10:10', '0'),
+(DEFAULT, '9', 'Lista de compras', '1) Pan 2) Manteca 3) Polenta 4) Yogurt', '0', '2020-01-01 10:10:10', '2020-01-01 10:10:10', '0'),
+(DEFAULT, '10', 'Cumpleanos', '1) 26/06 = Juanito 2) 11/05 = Menganito 3) 01/04 = Felipe', '0', '2020-01-01 10:10:10', '2020-01-01 10:10:10', '0');
 
 SELECT * FROM notas;
 
