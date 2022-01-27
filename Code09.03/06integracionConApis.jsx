ba8887cb432fccd04a03ea9f5dbf6a04bfce852d
Criptonyta <daninavaza@gmail.com>
2022-01-27 20:00:12 -0300

    Mod: 09.03.06

--- Code09.03/06integracionConApis.jsx
+++ Code09.03/06integracionConApis.jsx
@@ -6,5 +6,23 @@ Con esta funcionalidad a mano, vamos a poder incluso hacer peticiones a la API c
 
 BIBLIOTECAS AJAX => Con react podemos usar cualquier biblioteca AJAX, las mas populares son Axios, JQuery AJAX, windows.fetch
 
-
+componentDidMount() => Con este metodo que identifica el ciclo de vida del componente, JUSTO DESPUES DE LA PRIMERA VEZ QUE FUE RENDERIZADO, es donde debemos buscar la informacion que queremos de la API 
 */
+
+// configuramos el estado del componente para que pueda almacenar la informacion de la API luego de hacer el fetch
+class Gif extends Component {
+    constructor(props) {
+        super(props)
+        this.state = {
+            git: ""
+        }
+    }
+
+// ejecutamos el fetch() hacia el endpoint donde esta la informacion. Una vez procesada la data con .json, la misma estara lista para ser utilizada. Por eso, en el segundo .then(), asignamos esa informacion al estado del componente que habia declarado previamente 
+    componentDidMount(){
+        fetch('ENDPONIT DE LA API')
+        .then(response => {response.json})
+        .then(data=>)
+    }
+
+}
