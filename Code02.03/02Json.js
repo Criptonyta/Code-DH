9c44cc142fe12b4188884ac31123e54b63f7e8e4
Daniel <daninavaza@yahoo.com>
2022-10-12 23:37:08 -0300

    Add

--- Code02.03/02Json.js
+++ Code02.03/02Json.js
@@ -1,17 +1,17 @@
-// Jason sirve para estandarizar en un formato transaccional, la informacion en una cadena de texto para poder utilizarla en otros lenguajes
+// Json sirve para estandarizar en un formato transaccional, la informacion en una cadena de texto para poder utilizarla en otros lenguajes
 
 
 // Si tengo un array Js
-let amigos = ['Daniel', 'Javier','Pedro'];
-console.log (amigos);
+let amigos = ['Daniel', 'Javier', 'Pedro'];
+console.log(amigos);
 
 // Stringify = para convertir el array a formato Json y guardarlo en una nueva variable
 let amigosJson = JSON.stringify(amigos);
-console.log (amigosJson);
+console.log(amigosJson);
 
 // Parse = Si tengo un string en formato Json y quiero llevarlo nuevamente a formato original
 let amigosOriginal = JSON.parse(amigosJson);
-console.log (amigosOriginal);
+console.log(amigosOriginal);
 
 
 // Tambien se puede utilizar estos metodos con Objetos
@@ -20,7 +20,7 @@ let persona = {
     edad: 26,
     domicilio: "Calle falsa 123"
 };
-console.log (persona);
+console.log(persona);
 
 let personaJSON = JSON.stringify(persona);
 console.log(personaJSON);
@@ -30,5 +30,4 @@ console.log(personaOriginal);
 
 
 let fraseOriginal = 'Hola mundo';
-let textoBuscado = 'Hola'; 
-
+let textoBuscado = 'Hola';
